138c74728ea1f4f3dfa18d7c737d53769c16f573
DESKTOP-GTEC5V2\devzain <geekspod@gmail.com>
2018-07-23 17:10:31 +0500

    Bootstrap 4 Added!

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,41 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
-var prettify = require('gulp-prettify');
-var minify = require('gulp-minify');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 // Compile & Generate Style File
 gulp.task('styles', function () {
-    gulp.src('./src-assets/scss/style.scss')
+    gulp.src(['src-assets/scss/bootstrap-components/bootstrap.scss','./src-assets/scss/style.scss'])
         .pipe(sass())
         .pipe(cleanCSS({compatibility: 'ie8'})) //Later on
         .pipe(gulp.dest('./'));
 });
 
 
-// gulp.task('woocommerce', function () {
-//     gulp.src('../../wp-content/plugins/woocommerce/assets/css/woocommerce.scss')
-//         .pipe(sass())
-//         .pipe(cleanCSS({compatibility: 'ie8'})) //Later on
-//         .pipe(gulp.dest('../../wp-content/plugins/woocommerce/assets/css/'));
-//        // .pipe(gulp.dest('../../wp-content/themes/mall-road/'));
-// });
-
 // Compile & Generate Javscript File
 gulp.task('scripts', function() {
     return gulp.src([
 
-        //jQuery
-        //'javascripts/vendor-components/jquery/jquery.1.12.4.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
 
-        //Bootstrap Components
+        //----- Bootstrap Components ------------------
 
-        //'javascripts/bootstrap-components/affix.js',
-        //'javascripts/bootstrap-components/alert.js',
+        'src-assets/javascripts/bootstrap-components/alert.js',
         'src-assets/javascripts/bootstrap-components/button.js',
         'src-assets/javascripts/bootstrap-components/carousel.js',
         'src-assets/javascripts/bootstrap-components/collapse.js',
         'src-assets/javascripts/bootstrap-components/dropdown.js',
         'src-assets/javascripts/bootstrap-components/modal.js',
-        //'javascripts/bootstrap-components/popover.js',
-        //'javascripts/bootstrap-components/scrollspy.js',
+        'src-assets/javascripts/bootstrap-components/popover.js',
+        'src-assets/javascripts/bootstrap-components/scrollspy.js',
         'src-assets/javascripts/bootstrap-components/tab.js',
-        //'javascripts/bootstrap-components/tooltip.js',
-        'src-assets/javascripts/bootstrap-components/transition.js',
+        'src-assets/javascripts/bootstrap-components/tooltip.js',
+        'src-assets/javascripts/bootstrap-components/util.js',
 
 
 
+        //----- Bootstrap Components Ends ------------------
 
         //Common Components
         'src-assets/javascripts/common-components/global.js',
